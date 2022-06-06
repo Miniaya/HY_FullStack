7ebbc3201724273d2c5949520df50b7b5d7689f9
Henna Mäkiluoma <henna.makiluoma@futurice.com>
2022-06-06 20:51:12 +0300

    ex. 9.19

--- osa9/patientor-backend/src/types.ts
+++ osa9/patientor-backend/src/types.ts
@@ -10,8 +10,45 @@ export enum Gender {
   Other = 'other'
 }
 
-export interface Entry {
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
 
+type Discharge = {
+  date: string;
+  criteria: string;
+}
+
+type SickLeave = {
+  startDate: string;
+  endDate: string;
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
 }
 
 export interface Patient {
@@ -23,6 +60,11 @@ export interface Patient {
   occupation: string;
   entries: Entry[];
 }
+
+export type Entry = 
+| HospitalEntry
+| OccupationalHealthcareEntry
+| HealthCheckEntry
 
 export type NewPatient = Omit<Patient, 'id'>;
 
